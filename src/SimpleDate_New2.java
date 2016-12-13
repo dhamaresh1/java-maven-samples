b3e2036d586309212867721eac75d725b3b1f8f8
Dhamresh Kotha <kumar2k88@yahoo.com>
2016-12-13 21:38:42 +0000

    added new file_2 and changes to old file for changelog plugin_5

--- src/SimpleDate_New2.java
+++ src/SimpleDate_New2.java
@@ -1,20 +1,13 @@
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class SimpleDate {
+public class SimpleDate_New2 {
 	 public static void main(String[] args){
 	    Date date = new Date();
 	    String strDateFormat = "HH:mm:ss a";
 	    SimpleDateFormat sdf = new SimpleDateFormat(strDateFormat);
 	    System.out.println(sdf.format(date));
-/*	Test1 2 3 4*/
-/*	Test 5 6 7 8*/
-/*	Test Change log 1 2 3 added along side new file*/
-/*	Test Change log 1 2 3 4 added along side new file_1*/
-/*	Test Change log 1 2 3 4 added along side new file_2*/
-
-
+/*	For change log testing new file_2/
 
 	   }
 	}
