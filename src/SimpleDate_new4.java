d311cde93b28c8732531d32e991e138da5ad8b01
Dhamresh Kotha <kumar2k88@yahoo.com>
2017-01-01 20:05:45 +0000

    added new_4, deleted new_1 files

---
+++ src/SimpleDate_new4.java
@@ -0,0 +1,15 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SimpleDate_New {
+	 public static void main(String[] args){
+	    Date date = new Date();
+	    String strDateFormat = "HH:mm:ss a";
+	    SimpleDateFormat sdf = new SimpleDateFormat(strDateFormat);
+	    System.out.println(sdf.format(date));
+/*	Test1 2 3 4*/
+/*	Test 5 6 7 8*/
+/*	Test Change log 1 2 */
+
+	   }
+	}
--- src/buildShell_local.sh
+++ src/buildShell_local.sh
@@ -2,19 +2,28 @@
 # SCRIPT:  method1.sh
 # PURPOSE: Process a file line by line with PIPED while-read loop.
 #FILENAME=$1
-cat git_Changelog_local.txt | while read Commits
-do
-  echo cmt1=${Commits:16:40}
-  echo cmt2=${Commits:61:40}
-  git show --pretty="format:" --name-only $cmt1 $cmt2 | sort | uniq > Commits1.txt
-  mkdir -p ./${Commits:61:40}
-    cat Commits1.txt | while read LINE
-    do
-      echo "$count" "$LINE"
-    done
-  cd ${Commits:61:40}
 
-done
+head -1 git_Changelog_local.txt > firstLine.txt
+echo firstLine.txt
+
+#mkdir -p ./${Commits:61:40}
+#cd ${Commits:61:40}
+
+#while read Commits
+#do
+#  echo cmt1=${Commits:16:40}
+#  echo cmt2=${Commits:61:40}
+  #mkdir -p ./${Commits:61:40}
+  #cd ${Commits:61:40}
+#  pwd
+#  git show --pretty="format:" --name-only $cmt1 $cmt2 | sort | uniq > Commits1.txt
+#  cat Commits1.txt
+#    cat Commits1.txt | while read LINE
+#    do
+#      echo "$count" "$LINE"
+#    done
+#done
+
 
 #count=0
 #cd ./${Commits:61:40}
