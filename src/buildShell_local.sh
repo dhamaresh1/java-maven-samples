ddc0b418e6fea094328de385646bc9711cdc67a4
Dhamresh Kotha <kumar2k88@yahoo.com>
2017-01-04 12:47:42 +0000

    adding gitignore file

--- src/buildShell_local.sh
+++ src/buildShell_local.sh
@@ -1,57 +1,40 @@
 #!/bin/bash
-# SCRIPT:  method1.sh
-# PURPOSE: Process a file line by line with PIPED while-read loop.
-#FILENAME=$1
-rm -rf commotfile
+rm -rf commitfile
 temp_commits=$(mktemp commitfile)
-
 head -1 git_Changelog_local.txt > $temp_commits
-
-
-#mkdir -p ./${temp_commits:61:40}
-#cd ${Commits:61:40}
 
 cat $temp_commits | while read Commits
 do
   cmt1=${Commits:16:40}
   cmt2=${Commits:61:40}
-  echo "$cmt1"
-  echo "$cmt2"
+  mkdir -p ./Commit_History/"$cmt2"/Files_New
+  mkdir -p ./Commit_History/"$cmt2"/Files_Modified
+  mkdir -p ./Commit_History/"$cmt2"/Files_Deleted
 
-  mkdir -p ./"$cmt2"
-  git diff --name-status ${Commits:16:40} ${Commits:61:40} | sort | uniq > ./"$cmt2"/Commits1.txt
-  pwd
-  #cat Commits1.txt
-  cd ./"$cmt2"
-  pwd
-  echo "Sssssss"
-  cat Commits1.txt | while read LINE
-    do
-echo "yyyyyyy"
-      echo Mode=${LINE:0:1}
-      echo FILENAME=${LINE:2:99}
-      if [ "${LINE:0:1}" = "D" ]
-      then
-          pwd
-          echo "Mode is D"
-      elif [ "${LINE:0:1}" = "M"  ]
-      then
-        echo "Mode is M"
-      else
-        echo "Nothing for you"
-      fi
+  cd ../
+  git diff --name-status "$cmt1" "$cmt2" | sort | uniq > ./src/Commit_History/"$cmt2"/Commits1.txt
+  cat ./src/Commit_History/"$cmt2"/Commits1.txt | while read LINE
+      do
+        fileMode="$( cut -c 1 <<< "$LINE" )"
+        filePath="$( cut -d '/' -f 1 <<< "${LINE:2:49}" )"
+        fileName="$( cut -d '/' -f 2,2 <<< "$LINE" )"
+
+        if [ "$fileMode" = "A" ]
+        then
+          cp ./"$filePath"/"$fileName" ./src/Commit_History/"$cmt2"/Files_New
+          echo "$fileName has been added"
+        elif [ "$fileMode" = "D"  ]
+        then
+          echo /"$filePath"/"$fileName" > ./src/Commit_History/"$cmt2"/Files_Deleted/deletedFileList
+          echo "$fileName has been deleted"
+        elif [ "$fileMode" = "M"  ]
+        then
+          cp ./"$filePath"/"$fileName" ./src/Commit_History/"$cmt2"/Files_Modified
+          echo "$fileName has been Modified"
+        else
+          echo "Please Check something else happened with the file $fileName"
+        fi
     done
 done
 
 rm -rf ${temp_commits}
-
-
-#count=0
-#cd ./${Commits:61:40}
-#ls
-#cat Commits1 | while read LINE
-#do
-#let count++
-#echo "$count" "$LINE"
-#done
-#echo -e "\nTotal $count Lines read"
